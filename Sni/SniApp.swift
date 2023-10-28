280ac3e1b690e42fd9aa2572170cefc140b034e7
Konstantin Rudenskii <atrd.konstantin@gmail.com>
2023-10-28 18:48:30 +0300

    Initial Commit

---
+++ Sni/SniApp.swift
@@ -0,0 +1,17 @@
+//
+//  SniApp.swift
+//  Sni
+//
+//  Created by Konstantin Rudenskii on 28.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
